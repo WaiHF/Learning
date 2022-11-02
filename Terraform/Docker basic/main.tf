8d98d907a2973740cd96a29e4ec98dd7b92fefc6
Wai <waihfun@gmail.com>
2022-11-02 13:14:40 +0000

    Terraform docker tutorial

--- Terraform/Docker basic/main.tf
+++ Terraform/Docker basic/main.tf
@@ -1,13 +1,5 @@
-terraform {
-  required_providers {
-    docker = {
-      source  = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-    }
-  }
-}
-
 provider "docker" {}
+provider "random" {}
 
 resource "docker_image" "nginx" {
   name         = "nginx:latest"
@@ -16,9 +8,29 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = var.container_name
+  name  = "hello-terraform"
   ports {
     internal = 80
-    external = 8080
+    external = 8000
   }
+}
+
+resource "random_pet" "dog" {
+  length = 2
+}
+
+module "nginx-pet" {
+  source = "./nginx"
+
+  container_name = "hello-${random_pet.dog.id}"
+  nginx_port = 8001
+}
+
+module "hello" {
+  source  = "joatmon08/hello/random"
+  version = "3.0.1"
+
+  hello = random_pet.dog.id
+
+	secret_key = "secret"
 }
